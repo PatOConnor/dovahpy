33a416b764656531d86cd17db7a76cd9c991e8f0
PatOConnor <patoconnorma@gmail.com>
2022-02-10 13:26:39 -0500

    done with inventory grab (with 2 caveats)

--- dovah/alchemist.py
+++ dovah/alchemist.py
@@ -5,6 +5,10 @@ import keyboard
 import pygetwindow as gw
 from time import sleep
 from pytesseract import pytesseract as tess
+from rich import print
+import re
+from skyrimdata.skyrimalchemy import skyrim_alchemy
+
 tess.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 import ctypes
 user32 = ctypes.windll.user32
@@ -16,60 +20,138 @@ def take_screenshot():
     left = 0.14*screensize[0]
     top = 0
     height = screensize[1]
-    width = 0.3*screensize[0]
+    width = 0.25*screensize[0]
     image = pyautogui.screenshot(region=(left, top, height, width))
     image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     cv2.imwrite('image.png', image)
     return image
 
-def magnify_skyrim():
-    win = gw.getWindowsWithTitle('Skyrim Special Edition')[0]
-    win.activate()
-    #win.maximize()
+def magnify(window):
+    if window == 'skyrimSE':
+        win = gw.getWindowsWithTitle('Skyrim Special Edition')[0]
+        win.activate()
+        #win.maximize()
+    elif window == 'shell':
+        win = gw.getWindowsWithTitle('Windows PowerShell')[0]
+        win.activate()
 
-def scroll_down(amt):
+def scroll(amt,direction):
     for i in range(amt):
-        keyboard.press_and_release('s')
-        sleep(0.25)
+        keyboard.press_and_release(direction)
+        sleep(0.15)
 
-def filter_misreads(text):
+def read_alchemy_list(inv_list):
+    #pattern = re.compile(r"([a-zA-Z]+( [a-zA-Z]+)+) ?(\([0-9]+\))?")
+    pattern = re.compile(r"[a-zA-Z ']+(\([0-9]+\))?")
+    true_list = []
+    sus_list = []
+    for item in inv_list:
+        if item and item[0] == '`':
+            item = item[1::]
+        #valid skyrim item
+        if pattern.match(item, re.IGNORECASE):
+            paren_index = item.find('(')
+            if paren_index > 1:
+                q = item[paren_index+1:item.find(')'):]
+                q = int(q)
+                n = item[:paren_index:]
+            else:
+                n = item
+                q = 1
+            true_list.append([n, q])
+    return true_list
 
 def image_to_list(img):
     inv_list = []
     text = tess.image_to_string(img).split('\n')
-    #text = filter_misreads(text)
     print(len(text),text[0],text[len(text)-1])
-    return [1,2]
+    return text
+
+def gather(direction):
+    inv_list = []
+    img = take_screenshot()
+    inv_page = image_to_list(img)
+    prev_page = inv_page
+    inv_list.extend(inv_page)
+    while(True):
+        scroll(8,direction)
+        sleep(.3)#motion blur
+        img = take_screenshot()
+        inv_page = image_to_list(img)
+        inv_list.extend(inv_page)
+        #stops when it goes twice in a row
+        if inv_page[0] == prev_page[0]:
+            break
+        else:
+            prev_page = inv_page
+
+    #print('before conversion: ', inv_list)
+    inv_list = list(set(inv_list))#remove duplicates
+    inv_list.sort()
+    #print('after conversion: ', inv_list)
+    return inv_list
+
+def filter_inventory(inv_list):
+    true_list = []
+    sus_list = []
+    for item in inv_list:
+        for ingr in skyrim_alchemy:
+            if item[0].lower().strip() == skyrim_alchemy[ingr]['NAME'].lower():
+                true_list.append(item)
+    for item in inv_list:
+        if item not in true_list:
+            sus_list.append(item)
+    return true_list, sus_list
+
+def ask_user(sus_list):
+    reformed_list = []
+    for item in sus_list:
+        print(item[0])
+        dup = input('is this item a duplicate of one above? y/n ')
+        if dup.lower() == 'y':
+            continue
+        else:
+            correct = input('enter the correct name for this item')
+            reformed_list.append(correct, item[1])
 
 def gather_inventory():
     i = 0
-    magnify_skyrim()
+    magnify('skyrimSE')
     sleep(1)
-    scroll_down(9)#crop the top of the inventory
-    while(True):
-        inv_list = []
-        img = take_screenshot()
-        scroll_down(16)
-        inv_page = image_to_list(img)
-        inv_list.extend(inv_page)
-        #if it hasn't gotten to the end of the list
-        #if inv_page[-1] != inv_list[-1]:      <=actual condition
-        i += 1
-        if i == 3:
-            break
-    inv_list = list(set(inv_list))#remove duplicates
-
+    final_list = []
+    for i in range(2):
+        list1 = gather('s')
+        scroll(3, 'w')
+        list2 = gather('w')
+        inv_list = list1
+        inv_list.extend(list2)
+        inv_list = list(set(inv_list))
+        inv_list.sort()
+        final_list.extend(inv_list)
+    final_list = list(set(final_list))
+    final_list.sort()
+    #final_item = input('what is the last item in your inventory? ')
+    final_list.append('white cap')
+    final_list = read_alchemy_list(final_list)
+    print(final_list)
+    confirmed_ingr, sus_ingr = filter_inventory(final_list)
+    print(confirmed_ingr, sus_ingr)
+    print(len(final_list), len(confirmed_ingr), len(sus_ingr))
+    reformed_ingr = ask_user(sus_ingr)
+    if reformed_ingr:
+        confirmed_ingr.extend(reformed_ingr)
+    return confirmed_ingr
 
 def crunch_inventory(inv_list):
     pass
 
 def run():
     print('Alchemist Engine. Boot Skyrim to inventory menu and press any key.')
-    input()
+    #input()
     ingredients = gather_inventory()
-    potions_list = crunch_inventory(ingredients)
-    for p in potions_list:
-        print(p, potions_list[p])
+    #potions_list = crunch_inventory(ingredients)
+    #for p in potions_list:
+    #    print(p, potions_list[p])
 
 if __name__=="__main__":
     run()
